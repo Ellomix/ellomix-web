efcab161b744883e94eadce3ad7b9b2e6f62d3ec
Kevin Avila <kevin.avila@utexas.edu>
2020-01-02 23:18:55 -0600

    Side nav works properly at all screen positions

--- app/js/index.js
+++ app/js/index.js
@@ -17,36 +17,40 @@ window.onload = function() {
 	loadPage();
 };
 
-function loadPage(hideNav) {
+function loadPage() {
+	// Grab hash and load appropriate template
 	var hash = window.location.hash.slice(1).toLowerCase() || home;
 	var template = window.ellomix.templates[hash];
-	var contentDiv = $('#content');
-
+	
+	// Close side nav and set screen to the top
 	$('.side-nav').width(0);
-	showContent();
+	window.scrollTo(0, 0);
+
+	// Remove hidden class on elements
+	$('#content').removeClass('hidden')
+	$('.ellipse').removeClass('hidden')
+	$('.logo-text').removeClass('hidden')
+	$('.footer').removeClass('hidden')
+
+	// Set template HTML on content div
+	var contentDiv = $('#content');
 	contentDiv.html(template);
 }
 
 function openNav() {
 	$('.side-nav').animate({width: '100%'}, 500);
-	hideContent();
+
+	$('#content').removeClass('shown').addClass('hidden');
+	$('.ellipse').removeClass('shown').addClass('hidden');
+	$('.logo-text').removeClass('shown').addClass('hidden');
+	$('.footer').removeClass('shown').addClass('hidden');
 }
 
 function closeNav() {
 	$('.side-nav').animate({width: 0}, 500);
-	showContent();
-}
 
-function showContent() {
-	$('#content').show();
-	$('.ellipse').show();
-	$('.logo-text').show();
-	$('.footer').show();
-}
-
-function hideContent() {
-	$('#content').hide();
-	$('.ellipse').hide();
-	$('.logo-text').hide();
-	$('.footer').hide();
+	$('#content').removeClass('hidden').addClass('shown');
+	$('.ellipse').removeClass('hidden').addClass('shown');
+	$('.logo-text').removeClass('hidden').addClass('shown');
+	$('.footer').removeClass('hidden').addClass('shown');
 }

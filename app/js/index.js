a721cad5b9b11ee68ca00d3338cb880b1119cc91
Kevin Avila <kevin.avila@utexas.edu>
2020-02-13 21:21:42 -0600

    Centered subscribed text

--- app/js/index.js
+++ app/js/index.js
@@ -1,9 +1,6 @@
 "use strict";
 
-var home       = 'home';
-var subscribed = 'subscribed';
-var about      = 'about';
-var team       = 'team';
+var home = 'home';
 
 window.onpopstate = function() {
 	loadPage();

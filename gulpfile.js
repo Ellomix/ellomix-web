f5c0a0138a6c45d879c6d259030b647f14c48393
Kevin Avila <kevin.avila@utexas.edu>
2023-11-05 19:56:00 -0600

    Updated to gulp 4, added Alchemy '23 Laurel House and Alchemy 2.0 events, and updated social links

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var cssnano     = require('gulp-cssnano');
 var htmlmin     = require('gulp-htmlmin');
 var imagemin    = require('gulp-imagemin');
 var sourcemaps  = require('gulp-sourcemaps');
-var connect     = require('gulp-connect');
+var browsersync = require('browser-sync').create();
 var handlebars  = require('gulp-handlebars');
 var wrap        = require('gulp-wrap');
 var declare     = require('gulp-declare');
@@ -14,64 +14,65 @@ var del         = require('del');
 
 // Tasks 
 
-gulp.task('connect', function() {
-    connect.server({
-        root: 'dist',
-        port: 8001,
-        livereload: true
-    });
-});
+function browserSync() {
+  browsersync.init({
+      server: {
+          baseDir: 'dist'
+      },
+      port: 8001
+  });
+}
 
-gulp.task('watch', function() {
-	gulp.watch('app/css/*.css', ['css']);
-	gulp.watch('app/*.html', ['html']);
-  gulp.watch('app/templates/*.handlebars', ['templates']);
-	gulp.watch('app/js/*.js', ['js']);
-});
+function watch() {
+  gulp.watch('app/css/*.css', css);
+  gulp.watch('app/*.html', html);
+  gulp.watch('app/templates/*.handlebars', templates);
+  gulp.watch('app/js/*.js', js);
+}
 
-gulp.task('js', function() {	
-	return gulp.src('app/js/*.js')
-		.pipe(sourcemaps.init())
-		.pipe(concat('main.min.js'))
-		.pipe(uglify())
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('dist/js'))
-    .pipe(connect.reload());
-});
+function js() {
+  return gulp.src('app/js/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/js'))
+    .pipe(browsersync.stream());
+}
 
-gulp.task('css', function() {
-  gulp.src('app/css/*.css')
- 		.pipe(sourcemaps.init())
-  	.pipe(concat('style.css'))
-  	.pipe(cssnano())
-  	.pipe(sourcemaps.write())
-  	.pipe(gulp.dest('dist/css'))
-  	.pipe(connect.reload());
-});
+function css() {
+  return gulp.src('app/css/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(concat('style.css'))
+    .pipe(cssnano())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browsersync.stream());
+}
 
-gulp.task('images', function() {
-  gulp.src('app/img/**/*.+(png|jpg|gif|svg)')
+function images() {
+  return gulp.src('app/img/**/*.+(png|jpg|gif|svg)')
     .pipe(cache(imagemin()))
-    .pipe(gulp.dest('dist/img'))
-});
+    .pipe(gulp.dest('dist/img'));
+}
 
-gulp.task('videos', function() {
-  gulp.src('app/img/*.+(mp4)')
-    .pipe(gulp.dest('dist/img'))
-});
+function videos() {
+  return gulp.src('app/img/*.+(mp4)')
+    .pipe(gulp.dest('dist/img'));
+}
 
-gulp.task('html', function() {
-  gulp.src(['app/**/*.html'])
-  		.pipe(htmlmin({
-				collapseWhitespace: true,
-				removeComments: true
-			}))
-      .pipe(gulp.dest('dist'))
-      .pipe(connect.reload());
-});
+function html() {
+  return gulp.src(['app/**/*.html'])
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      removeComments: true
+    }))
+    .pipe(gulp.dest('dist'))
+    .pipe(browsersync.stream());
+}
 
-gulp.task('templates', function() {
-  gulp.src('app/templates/*.handlebars')
+function templates() {
+  return gulp.src('app/templates/*.handlebars')
     .pipe(handlebars())
     .pipe(wrap('Handlebars.template(<%= contents %>)'))
     .pipe(declare({
@@ -80,14 +81,24 @@ gulp.task('templates', function() {
     }))
     .pipe(concat('templates.js'))
     .pipe(gulp.dest('dist/js'))
-    .pipe(connect.reload());
-});
+    .pipe(browsersync.stream());
+}
 
-gulp.task('clean:dist', function() {
-	return del.sync('dist');
-})
+function clean() {
+  return del(['dist']);
+}
+
+exports.browserSync = browserSync;
+exports.watch       = watch;
+exports.js          = js;
+exports.css         = css;
+exports.images      = images;
+exports.videos      = videos;
+exports.html        = html;
+exports.templates   = templates;
+exports.clean       = clean;
 
 // Build Sequences
 
-gulp.task('default', ['clean:dist', 'html', 'templates', 'css', 'images', 'videos', 'js', 'connect', 'watch']);
-gulp.task('build', ['clean:dist', 'html', 'templates', 'css', 'images', 'videos', 'js']);
+exports.default = gulp.series(clean, html, templates, css, images, videos, js, browserSync, watch);
+exports.build = gulp.series(clean, html, templates, css, images, videos, js);
